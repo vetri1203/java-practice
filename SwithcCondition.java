c7e70a3071e2a161a201b27c3b3282ce99b4a67a
vetri1203 <vetriveljaganthan5125@gmail.com>
2025-06-28 13:27:36 +0530

    switch program added

---
+++ SwithcCondition.java
@@ -0,0 +1,16 @@
+public class SwithcCondition {
+    public static void main(String[] args)
+    {
+        String month = "Jan";
+
+        switch(month){
+            case "Jan":
+                System.out.println("This is Jan month");
+                break;
+            case "Feb":
+                System.out.println("This is Feb month");
+            default:
+            System.out.println("Unkown month");
+        }
+    }
+}
