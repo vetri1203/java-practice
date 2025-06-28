450ee31fdccc3874a58fba4eef035b1919e414de
vetri1203 <vetriveljaganthan5125@gmail.com>
2025-06-28 13:44:01 +0530

    while loop program added

---
+++ WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+    public static void main (String[] args)
+    {
+        int target = 50;
+        int index=1;
+        System.out.println("top of loop");
+
+        while (target>index) {
+            index++;
+            System.out.println("in loop");
+        }
+    }
+}
